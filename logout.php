0ca604fada063575f5155343323d591cca17474f
Mohamad Al Kodmani <46457940+kodmani@users.noreply.github.com>
2019-01-11 20:37:24 -0500

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,11 +1,11 @@
 <?php
-	session_start();
-	if(isset($_SESSION['Admin'])){
-	    session_unset();
-	    session_destroy();
-	    session_start();
-	    session_regenerate_id();
-	} 
+session_start();
+if(isset($_SESSION['Admin'])){
+    session_unset();
+    session_destroy();
+    session_start();
+    session_regenerate_id();
+} 
 
-    header('Location:login.php');
+header('Location:login.php');
 ?>
